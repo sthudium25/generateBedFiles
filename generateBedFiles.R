34d4654710a31fefb3b0b24d32d2ed6b0588b2e7
sthudium25 <sam.thudium1@gmail.com>
2022-02-03 10:47:27 -0500

    Adding functionaling for creating and outputting bed files depending on the user input. Options include genic, promoter (or both) coords for all genes or select genes (from gene list). Utility for mouse or human genomes

--- generateBedFiles.R
+++ generateBedFiles.R
@@ -1,4 +1,4 @@
-packages = c('tidyverse', 'biomaRt', 'rtracklayer', 'tools', 'readxl', 'biomaRt')
+packages = c('tidyverse', 'biomaRt', 'rtracklayer', 'tools', 'readxl', 'glue')
 suppressPackageStartupMessages(invisible(lapply(packages, library, character.only=T)))
 
 # -------------------------------------------------------
@@ -20,9 +20,9 @@ region = 'genic'
 upstream = 2000
 
 # ---------------------------------------------------------
+# DON'T ALTER THESE FUNCTIONS
 
 # This function trims the 5' end of genes if there is a cds within a specified bp distance 
-# DON'T ALTER WITH THIS FUNCTION
 trimFivePrimeSide <- function(gr, remove)
 {
   stopifnot(is(gr, "GenomicRanges"), is(remove, "GenomicRanges"))
@@ -51,6 +51,7 @@ trimFivePrimeSide <- function(gr, remove)
   gr
 }
 
+# This function loads the Transcript Database corresponding to your selected organism
 loadTxDbFromOrganism <- function(organism) {
   if (organism == 'mouse') {
     library(TxDb.Mmusculus.UCSC.mm10.knownGene)
@@ -63,15 +64,17 @@ loadTxDbFromOrganism <- function(organism) {
   txdb
 }
 
+# This function links to ensembl so that we can convert between different gene identifiers
 generateMartObject <- function(organism) {
   if (organism == 'mouse') {
-    ensembl = useMart(biomart = "ensembl", dataset = "mmusculus_gene_ensembl")
+    ensembl = useEnsembl(biomart = "genes", dataset = "mmusculus_gene_ensembl")
   } else if (organism == 'human') {
-    ensembl = useMart(biomart = 'ensembl', dataset = 'hsapiens_gene_ensembl')
+    ensembl <- useEnsembl(biomart = "genes", dataset = "hsapiens_gene_ensembl")
   }
   ensembl
 }
 
+# This funciton reads in your provided list of genes
 readInGeneList <- function(path) {
   ext = tools::file_ext(path)
   if (! (ext %in% c('txt', 'xlsx', 'csv'))) {
@@ -87,21 +90,84 @@ readInGeneList <- function(path) {
   out
 }
 
+# This is a helper function to retrieve ensembl ids and gene names given a list of entrez ids
+entrezToEnsemblPlusGeneSymbol <- function(organism) {
+  if (organism == 'human') {
+    query = getBM(attributes = c("entrezgene_id", 'hgnc_symbol', 'ensembl_gene_id'),
+                  filters = 'entrezgene_id',
+                  values = tmp$gene_id,
+                  mart = mart)
+  } else if (organism == 'mouse') {
+    query = getBM(attributes = c("entrezgene_id", 'mgi_symbol', 'ensembl_gene_id'),
+                  filters = 'entrezgene_id',
+                  values = tmp$gene_id,
+                  mart = mart)
+  }
+  query
+}
+
 txdb = loadTxDbFromOrganism(organism = organism)
 
 mart = generateMartObject(organism)
 
-
-createBEDs <- function(genes = c('all', 'select'), 
-                       genomic_loc) {
-  if (genes == 'all') {
-    
+createBEDs <- function(txdb, genes = c('all', 'select'), 
+                       genomic_loc = 'all',
+                       organism = 'mouse',
+                       upstream = 2000,
+                       genelistPath) {
+  gr = genes(txdb)
+  tmp = as_tibble(gr)
+  query = entrezToEnsemblPlusGeneSymbol(organism)
+  query$entrezgene_id = as.character(query$entrezgene_id)
+  
+  tmp = tmp %>% inner_join(query, by = c('gene_id' = 'entrezgene_id'))
+  forBed = tmp %>%
+    dplyr::select(-width, -gene_id) %>%
+    arrange(seqnames)
+  colnames(forBed)[5] = 'gene_symbol'
+  
+  if (genes == 'all' && genomic_loc == 'genic' || genomic_loc == 'both') {
+    forBed %>%
+      dplyr::select(-ensembl_gene_id) %>%
+      write_tsv(forBed, file = glue('outputs/{txdb$user_genome[1]}_allGenic.bed'))
+  }
+  forBed_gr = makeGRangesFromDataFrame(forBed, keep.extra.columns = T)
+  if (genes == 'all' && genomic_loc == 'promoter' || genomic_loc == 'both') {
+    geneFlanks = flank(forBed_gr, 
+                       width = upstream)
+    geneFlanksDF = as.data.frame(geneFlanks) 
+    cdsCoords = cds(txdb)
+    gnflankTrimmed = as_tibble(trimFivePrimeSide(geneFlanks, cdsCoords))
+    gnflankTrimmed %>%
+      dplyr::select(-width) %>%
+      write_tsv(file=glue('outputs/{txdb$user_genome[1]}_allPromoter{upstream}.bed'))
+  }
+  
+  # Figure out which type of IDs are being used in the selected genes list
+  selection_type = (grepl('ENSG', select_genes[1], fixed=T) || grepl('ENSMUS', select_genes[1], fixed=T))
+  
+  if (genes == 'select' && genomic_loc == 'genic' || genomic_loc == 'both') {
+    select_genes = readInGeneList(my_genes)
+    select_genes = select_genes[,1][[1]]
+    if (selection_type) { 
+      # this means we have ensembl ids in the user input gene list
+      select_genic = forBed %>%
+        filter(ensembl_gene_id %in% select_genes) %>%
+        dplyr::select(seqnames, start, end, strand, gene_symbol) %>%
+        dplyr::rename(name = gene_symbol, chrom = seqnames)
+      select_genic %>%
+        write_tsv(file = glue('outputs/{txdb$user_genome[1]}_{basename()}Genic.bed'))
+      
+    } else {
+      select_genic = 
+    }
+   
   }
 }
 
-select_genes = readInGeneList(my_genes)
 
-gr = genes(txdb)
-tmp = as_tibble(gr)
-query = getBM(attributes = c("entrezgene_id", 'hgnc_symbol'),
-              filters = )
+
+
+
+
+
